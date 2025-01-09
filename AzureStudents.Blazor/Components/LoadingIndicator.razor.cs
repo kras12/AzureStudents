336a6cab0b7ecc6b0f064023eda630b747e37f94
runehero <runehero@hotmail.com>
2025-01-09 21:55:25 +0100

    Added a loading indicator component.

---
+++ AzureStudents.Blazor/Components/LoadingIndicator.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AzureStudents.Blazor.Components;
+
+/// <summary>
+/// A component that displays a loading indicator. 
+/// </summary>
+public partial class LoadingIndicator : ComponentBase
+{
+
+}
