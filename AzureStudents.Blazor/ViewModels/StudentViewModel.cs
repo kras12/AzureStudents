a0a1eaab37ea897e63a01a3d8d506e96b1e53920
runehero <runehero@hotmail.com>
2025-01-09 21:37:18 +0100

    Added view models for listing students and user messages.

---
+++ AzureStudents.Blazor/ViewModels/StudentViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace AzureStudents.Blazor.ViewModels;
+
+/// <summary>
+/// Viewmodel for a student. 
+/// </summary>
+public class StudentViewModel
+{
+    /// <summary>
+    /// The first name of the student.
+    /// </summary>
+    public string FirstName { get; set; } = "";
+
+    /// <summary>
+    /// The ID of the student.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// The last name of the student. 
+    /// </summary>
+    public string LastName { get; set; } = "";
+}
