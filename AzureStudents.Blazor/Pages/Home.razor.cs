05b022d024a318739cb728144b31e8f3ee7cf8bf
runehero <runehero@hotmail.com>
2025-01-09 22:33:20 +0100

    Added a delay for development builds to give the backend container time to start up.

--- AzureStudents.Blazor/Pages/Home.razor.cs
+++ AzureStudents.Blazor/Pages/Home.razor.cs
@@ -7,5 +7,24 @@ namespace AzureStudents.Blazor.Pages;
 /// </summary>
 public partial class Home : ComponentBase
 {
+    #region Fields
 
+    /// <summary>
+    /// True if the component have been initialized. 
+    /// </summary>
+    private bool _isInitialized;
+
+    #endregion
+
+    /// <inheritdoc/>
+    protected override async Task OnInitializedAsync()
+    {
+#if DEBUG
+        // Let the backend container start first. 
+        await Task.Delay(500);
+#endif
+
+        await base.OnInitializedAsync();
+        _isInitialized = true;
+    }
 }
