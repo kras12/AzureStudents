2684032596ccd59afbba75a01c1d4e7c5001c46a
runehero <runehero@hotmail.com>
2025-01-09 21:59:08 +0100

    Added the home page component.

---
+++ AzureStudents.Blazor/Pages/Home.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AzureStudents.Blazor.Pages;
+
+/// <summary>
+/// Component for the home page.
+/// </summary>
+public partial class Home : ComponentBase
+{
+
+}
